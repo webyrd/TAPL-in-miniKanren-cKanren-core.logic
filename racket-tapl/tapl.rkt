65111ba56ec39a3c9d11b274dbb5df4b836d0db6
William E. Byrd <webyrd@gmail.com>
2013-05-30 02:17:11 -0600

    updated README and added expected test results

--- racket-tapl/tapl.rkt
+++ racket-tapl/tapl.rkt
@@ -12,7 +12,7 @@
       [(== 'z i) (== (empty-set) Si)]
       [(== '(s z) i)
        (fresh (Sa Sb)
-         (uniono (empty-set) (make-set '(true false 0)) Sa)
+         (uniono (empty-set) (make-set '(true false 0) Sa))
          (uniono Sa (make-set '(succ pred iszero)) Sb)
          (uniono Sb (make-set '(if)) Si))]
       [(fresh (i-1 i-2 Si-1 t1 t2 t3 Sa Sb)
@@ -27,7 +27,14 @@
          (uniono Sb (make-set `((if ,t1, t2 ,t3))) Si))])))
 
 ;;; I'm a Racket nub, and don't know how to properly turn these runs into tests
-(run* (q) (So 'z q))
-(run* (q) (So '(s z) q))
+(run* (q) (So 'z q)) ; => empty set
+(run* (q) (So '(s z) q)) ; => (true false 0 succ pred iszero if)
 (run* (q) (So '(s (s z)) q))
+; =>
+; (true false 0
+; (succ true) (succ false) (succ 0) (succ succ) (succ pred) (succ iszero) (succ if)
+; (pred true) (pred false) (pred 0) (pred succ) (pred pred) (pred iszero) (pred if)
+; (iszero true) (iszero false) (iszero 0) (iszero succ) (iszero pred) (iszero iszero) (iszero if)
+; (if true true true) (if true true false) ... (if if if if)
+; )
 (run* (q) (So '(s (s (s z))) q))
