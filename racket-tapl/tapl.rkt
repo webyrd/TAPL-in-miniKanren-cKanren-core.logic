5ef14f7f27f8cce89bcc0ff00e2b14c0c1957d94
William E. Byrd <webyrd@gmail.com>
2013-06-02 20:55:49 -0600

    cleaning up Racket code

--- racket-tapl/tapl.rkt
+++ racket-tapl/tapl.rkt
@@ -2,7 +2,11 @@
 
 ;;; Dependencies:  Racket and cKanren (https://github.com/calvis/cKanren)
 (require cKanren/miniKanren)
+;(require cKanren/neq)
+;(require cKanren/absento)
 (require cKanren/sets)
+
+(provide (all-defined-out))
 
 
 ; 3.2.3  Terms, concretely (p. 27)
@@ -10,13 +14,13 @@
 ; Adapted from @namin's https://github.com/namin/clpset-miniKanren/blob/master/tapl.scm
 ;
 ; Alas, this code is slowwwwwww
-(define S
+(define So
   (lambda (i s)
     (conde
       ((== 'z i) (== (empty-set) s))
       ((fresh (i-1 S-1 S-11 S-111 S-succ S-pred S-iszero S-if s1 s2 s3)
          (== `(s ,i-1) i)
-         (S i-1 S-1)
+         (So i-1 S-1)
          (map-seto (lambda (e o) (== `(succ ,e) o)) S-1 S-succ)
          (map-seto (lambda (e o) (== `(pred ,e) o)) S-1 S-pred)
          (map-seto (lambda (e o) (== `(iszero ,e) o)) S-1 S-iszero)
@@ -28,18 +32,18 @@
                        (== `(if ,e1 ,e2 ,e3) o)))
                    S-111
                    S-if)
-         (uniono (empty-set) (make-set '(true false zero)) S-succ s1)
+         (uniono (make-set '(true false zero)) S-succ s1)
          (uniono s1 S-pred s2)
          (uniono s2 S-iszero s3)
          (uniono s3 S-if s))))))
 
-(define mini-S
+(define mini-So
   (lambda (i s)
     (conde
      ((== 'z i) (== (empty-set) s))
      ((fresh (i-1 S-1 S-11 S-succ S-plus s1)
         (== `(s ,i-1) i)
-        (mini-S i-1 S-1)
+        (mini-So i-1 S-1)
         (map-seto (lambda (e o) (== `(succ ,e) o)) S-1 S-succ)
         (cartesiano S-1 S-1 S-11)
         (map-seto (lambda (e o)
@@ -77,74 +81,67 @@
          (uniono o1 or out))))))
 
 
-(run 1 (q) (map-seto (lambda (x out) (== `(,x) out))
-                     (make-set '(a b c))
-                     q))
-; '({ (a) (b) (c) | ∅ })
+(module+ test
+  (require cKanren/tester)
 
-(length
- (run 6 (q) (map-seto (lambda (x out) (== `(,x) out))
-                     (make-set '(a b c))
-                     q)))
-; 6
+  (test "map-seto-1"
+    (run 1 (q)
+      (map-seto
+       (lambda (x out)
+         (== `(,x) out))
+       (make-set '(a b c))
+       q))
+    (list (make-set '((a) (b) (c)))))
 
-;;; appears to diverge, as does run7
-(length
- (run* (q) (map-seto (lambda (x out) (== `(,x) out))
-                     (make-set '(a b c))
-                     q)))
-; 6
+  (test "map-seto-2"
+    (length
+     (run 6 (q)
+       (map-seto
+        (lambda (x out)
+          (== `(,x) out))
+        (make-set '(a b c))
+        q)))
+    6)
 
-(run 1 (q) (cartesiano (make-set '(a b c)) (make-set '(1 2 3)) q))
-; '((set ∅ (a 1) (a 2) (a 3) (b 1) (b 2) (b 3) (c 1) (c 2) (c 3)))
+  ;; (test "map-seto-3"
+  ;;   ;;; appears to diverge, as does run7
+  ;;   (length
+  ;;    (run* (q)
+  ;;      (map-seto
+  ;;       (lambda (x out) (== `(,x) out))
+  ;;       (make-set '(a b c))
+  ;;       q)))
+  ;;   6)
 
-(length (run* (q) (cartesiano (make-set '(a b)) (make-set '(1 2)) q)))
-; 1
+  ;; (test "cartesiano-1"
+  ;;   ;;; takes too long...
+  ;;   (run 1 (q) (cartesiano (make-set '(a b c)) (make-set '(1 2 3)) q))
+  ;;   '((set ∅ (a 1) (a 2) (a 3) (b 1) (b 2) (b 3) (c 1) (c 2) (c 3))))
 
- (run 1 (q) (S 'z q))
-; '(∅)
+  (test "cartesiano-2"
+    (length (run 1 (q) (cartesiano (make-set '(a b)) (make-set '(1 2)) q)))
+    1)
 
-(run 1 (q) (S '(s z) q))
-; '((set ∅ false true zero))
+  ;; (test "cartesiano-3"
+  ;;   ;;; takes too long...
+  ;;   (length (run* (q) (cartesiano (make-set '(a b)) (make-set '(1 2)) q)))
+  ;;   1)
 
-(run 1 (q) (mini-S '(s (s z)) q))
- ;; '((set ∅
- ;;    (plus false false) (plus false true) (plus false zero)
- ;;    (plus true false) (plus true true) (plus true zero)
- ;;    (plus zero false) (plus zero true) (plus zero zero)
- ;;    (succ false) (succ true) (succ zero) false true zero))
+  (test "So-1"
+    (run 1 (q) (So 'z q))
+    (list (empty-set)))
 
+  (test "So-2"
+    (run 1 (q) (So '(s z) q))
+    (list (make-set '(false true zero))))
 
-; 3.2.3  Terms, concretely (p. 27)
-; Defined using CLP(Set) and Peano numbers (z, (s z), (s (s z)), etc.)
-; This is super busted, since we'd need to perform setof to accumulate all the answers
-;; (define So
-;;   (lambda (i Si)
-;;     (conde
-;;       [(== 'z i) (== (empty-set) Si)]
-;;       [(== '(s z) i)
-;;        (fresh (Sa Sb)
-;;          (uniono (empty-set) (make-set '(true false 0)) Si))]
-;;       [(fresh (i-1 i-2 Si-1 t1 t2 t3 Sa Sb)
-;;          (== `(s ,i-1) i)
-;;          (== `(s ,i-2) i-1)
-;;          (So i-1 Si-1)
-;;          (ino t1 Si-1)
-;;          (ino t2 Si-1)
-;;          (ino t3 Si-1)
-;;          (uniono (empty-set) (make-set '(true false 0)) Sa)
-;;          (uniono Sa (make-set `((succ ,t1) (pred ,t1) (iszero ,t1))) Sb)
-;;          (uniono Sb (make-set `((if ,t1, t2 ,t3))) Si))])))
-
-;;; I'm a Racket nub, and don't know how to properly turn these runs into tests
-;; (run* (q) (So 'z q)) ; => empty set
-;; (run* (q) (So '(s z) q)) ; => (true false 0 succ pred iszero if)
-;; (run* (q) (So '(s (s z)) q))
-; =>
-; (true false 0
-; (succ true) (succ false) (succ 0) (succ succ) (succ pred) (succ iszero) (succ if)
-; (pred true) (pred false) (pred 0) (pred succ) (pred pred) (pred iszero) (pred if)
-; (iszero true) (iszero false) (iszero 0) (iszero succ) (iszero pred) (iszero iszero) (iszero if)
-; (if true true true) (if true true false) ... (if if if if)
-; )
-;; (run* (q) (So '(s (s (s z))) q))
+  ;; (test "mini-So-1"
+  ;;   ;;; takes too long
+  ;;   (run 1 (q) (mini-So '(s (s z)) q))
+  ;;   '((set ∅
+  ;;      (plus false false) (plus false true) (plus false zero)
+  ;;      (plus true false) (plus true true) (plus true zero)
+  ;;      (plus zero false) (plus zero true) (plus zero zero)
+  ;;      (succ false) (succ true) (succ zero) false true zero)))
+  
+)
--- scheme-tapl/tapl.scm
+++ scheme-tapl/tapl.scm
@@ -67,3 +67,4 @@
   )
 
 (import (tapl-ck))
+
